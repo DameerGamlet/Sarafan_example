cbbb6e0bc0b9ef14030a1e40e75591b4406b42b5
DameerGamlet <gubaidulin.dam@gmail.com>
2022-07-08 00:40:54 +0400

    update

--- src/main/java/com/example/sarafan/controllers/MessageController.java
+++ src/main/java/com/example/sarafan/controllers/MessageController.java
@@ -1,76 +1,47 @@
 package com.example.sarafan.controllers;
 
-import com.example.sarafan.exceptions.NotFoundException;
-import org.springframework.ui.Model;
+import com.example.sarafan.models.Message;
+import com.example.sarafan.models.Views;
+import com.example.sarafan.repositories.MessageRepository;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import javax.swing.text.View;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("message")
+@AllArgsConstructor
 public class MessageController {
-    private final List<Map<String, String>> mapList = new ArrayList<>() {
-        {
-            add(new HashMap<>() {{
-                put("id", "1");
-                put("text", "First");
-            }});
-            add(new HashMap<>() {{
-                put("id", "2");
-                put("text", "Second");
-            }});
-            add(new HashMap<>() {{
-                put("id", "3");
-                put("text", "Third");
-            }});
-            add(new HashMap<>() {{
-                put("id", "4");
-                put("text", "Fourth");
-            }});
-        }
-    };
-    int counter = mapList.size();
+
+    private final MessageRepository messageRepository;
 
     @GetMapping()
-    public List<Map<String, String>> getMessageList() {
-        return mapList;
+    @JsonView(Views.IdName.class)
+    public List<Message> getMessageList() {
+        return messageRepository.findAll();
     }
 
     @GetMapping("{id}")
-    public Map<String, String> getIneMessage(@PathVariable String id) {
-        return getMessage(id);
-    }
-
-    private Map<String, String> getMessage(String id) {
-        return mapList
-                .stream()
-                .filter(mapList -> mapList.get("id").equals(id))
-                .findFirst()
-                .orElseThrow(NotFoundException::new);
-    }
-
-    @PostMapping()
-    public Map<String, String> createMessage(@RequestBody Map<String, String> message) {
-        message.put("id", ++counter + "");
-        mapList.add(message);
+    public Message getMessage(@PathVariable("id") Message message) {
         return message;
     }
 
+    @PostMapping()
+    public Message createMessage(@RequestBody Message message) {
+        return messageRepository.save(message);
+    }
+
     @PutMapping("{id}")
-    public Map<String, String> updateMessage(@PathVariable String id, @RequestBody Map<String, String> message) {
-        System.out.println(id);
-        Map<String, String> messageFromDB = getMessage(id);
-        messageFromDB.putAll(message);
-        messageFromDB.put("id", id);
-        return messageFromDB;
+    public Message updateMessage(@PathVariable("id") Message messageFromDB, @RequestBody Message message) {
+        BeanUtils.copyProperties(message, messageFromDB, "id");
+        return messageRepository.save(messageFromDB);
     }
 
     @DeleteMapping("{id}")
-    public void deleteMessage(@PathVariable String id) {
-        Map<String, String> message = getMessage(id);
-        mapList.remove(message);
+    public void deleteMessage(@PathVariable("id") Message message) {
+        messageRepository.delete(message);
     }
 }
---
+++ src/main/java/com/example/sarafan/repositories/MessageRepository.java
@@ -0,0 +1,9 @@
+package com.example.sarafan.repositories;
+
+import com.example.sarafan.models.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
