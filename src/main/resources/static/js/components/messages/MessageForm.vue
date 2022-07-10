0f6cc2c44fdcacf17c38aad3173df96593520603
DameerGamlet <gubaidulin.dam@gmail.com>
2022-07-10 15:40:14 +0400

    add webpack

---
+++ src/main/resources/static/js/components/messages/MessageForm.vue
@@ -0,0 +1,57 @@
+<template>
+    <div style="margin-bottom: 20px;">
+        <input type="text" style="width: 200px;" placeholder="Write something" v-model="text" />
+        <input type="button" style="width: 80px;" value="Добавить" @click="save" />
+    </div>
+</template>
+
+<script>
+    function getIndex(list, id) {
+        for (var i = 0; i < list.length; i++ ) {
+            if (list[i].id === id) {
+                return i
+            }
+        }
+        return -1
+    }
+    export default {
+        props: ['messages', 'messageAttr'],
+        data() {
+            return {
+                text: '',
+                id: ''
+            }
+        },
+        watch: {
+            messageAttr(newVal, oldVal) {
+                this.text = newVal.text
+                this.id = newVal.id
+            }
+        },
+        methods: {
+            save() {
+                const message = { text: this.text }
+                if (this.id) {
+                    this.$resource('/message{/id}').update({id: this.id}, message).then(result =>
+                        result.json().then(data => {
+                            const index = getIndex(this.messages, data.id)
+                            this.messages.splice(index, 1, data)
+                            this.text = ''
+                            this.id = ''
+                        })
+                    )
+                } else {
+                    this.$resource('/message{/id}').save({}, message).then(result =>
+                        result.json().then(data => {
+                            this.messages.push(data)
+                            this.text = ''
+                        })
+                    )
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
