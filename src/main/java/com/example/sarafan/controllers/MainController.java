6489a44817040a543eb670573fb47832ad8486aa
DameerGamlet <gubaidulin.dam@gmail.com>
2022-07-10 15:39:41 +0400

    add webpack

--- src/main/java/com/example/sarafan/controllers/MainController.java
+++ src/main/java/com/example/sarafan/controllers/MainController.java
@@ -4,6 +4,7 @@ import com.example.sarafan.models.User;
 import com.example.sarafan.repositories.MessageRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +21,9 @@ public class MainController {
     @Autowired
     private final MessageRepository messageRepository;
 
+//    @Value("${spring.profiles.active}")
+//    private String profile;
+
     @GetMapping
     public String main(Model model,
                        @AuthenticationPrincipal User user){
@@ -29,6 +33,7 @@ public class MainController {
         data.put("messages", messageRepository.findAll());
 
         model.addAttribute("frontendData", data);
+        model.addAttribute("isDevMode", true);
         return "index";
     }
 }
